17dcac8da11ca5ace95703825f9cb899e165aa94
Hammad Tariq <hammadtariq0007@gmail.com>
2025-08-09 18:09:00 +0500

    Changed Styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 
 function App() {
   const [count, setCount] = useState(0);
+
   return (
     <div className='App'> 
-      <Navbar logoText = "LearningReact"/>
-      <h1>Welcome to My First App</h1>
-      <p>This is a simple React application.</p>
-      <p>Feel free to explore and modify the code!</p>
-      <p>Happy coding!</p>
-      <div>{count}</div>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <Navbar logoText="LearningReact"/>
+      
+      <div className="container">
+        <h1>Welcome to My First App</h1>
+        <p>This is a simple React application.</p>
+        <p>Feel free to explore and modify the code!</p>
+        <p>Happy coding!</p>
+        <span>{count}</span>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+
       <Footer/>
     </div>
   );
